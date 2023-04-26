1a504dc5cf29e3d1fd58437f09a7f1a8c810909f
kadetete <kadetesan@gmail.com>
2023-04-26 07:59:59 -0300

    tentativa de resolução da questão 1 U1 - F2

--- funcoes_dbbiblioteca/u1_f2_udfs.sql
+++ funcoes_dbbiblioteca/u1_f2_udfs.sql
@@ -1,3 +1,17 @@
+-- Questão 1
+DELIMITER $$
+CREATE OR REPLACE FUNCTION fnRele(cmd VARCHAR(4))
+RETURNS VARCHAR(4) DETERMINISTIC
+BEGIN
+	DECLARE msg VARCHAR(50);
+    IF cmd = "LOW" THEN SET msg = "Relé foi acionado.";
+    ELSEIF cmd = "HIGH" THEN SET msg = "Relé foi desativado";
+    ELSE SET msg = "Comando inválido";
+    END IF;
+    RETURN msg;
+END $$
+DELIMITER ;
+
 -- Questão 2
 DELIMITER $$
 CREATE OR REPLACE FUNCTION fnAplicaDesconto(preco NUMERIC(9, 2), desconto NUMERIC(5, 2))
