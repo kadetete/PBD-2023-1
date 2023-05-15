7f8296a9a5aa11a602a0077114c93940aaed204f
kadetete <kadetesan@gmail.com>
2023-05-15 09:07:27 -0300

    Q02 de ex02 dbplaylist

--- db_playlist/procedures/ex02_diagramas.sql
+++ db_playlist/procedures/ex02_diagramas.sql
@@ -6,3 +6,12 @@ SELECT CoPessoa, InTipoPessoa, NoPessoa
 FROM tbPessoa;
 
 SELECT * FROM vwPessoa;
+
+-- Q 02
+
+CREATE OR REPLACE VIEW vwMusico
+AS
+SELECT CoMusico, CoBanda, InVocal, VaSalario
+FROM tbMusico;
+
+SELECT * FROM vwMusico;
